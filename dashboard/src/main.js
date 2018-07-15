199a74c18b292f941a8d88c7a7185385ee7cd1da
Laurent Vaylet <laurent.vaylet@gmail.com>
2018-07-15 16:24:28 +0200

    adjust doc

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -7,7 +7,7 @@ import VueMqtt from 'vue-mqtt'
 // DEVELOPMENT
 // Publish messages from MacOS with:
 // $ brew install mosquitto
-// $ mosquitto_pub -h iot.eclipse.org -t "/drone/position" -m "{"ts": [1531604906], "uuid": "7ebf7ece-1332-44ad-8f3a-db72245eded5", "name": "drone1", "latitude": 48.85150569993735, "longitude": 2.2119310978842477}"
+// $ mosquitto_pub -h iot.eclipse.org -t "drone/position" -m "{"ts": [1531604906], "uuid": "7ebf7ece-1332-44ad-8f3a-db72245eded5", "name": "drone1", "latitude": 48.85150569993735, "longitude": 2.2119310978842477}"
 Vue.use(VueMqtt, 'ws://iot.eclipse.org:80/ws')
 
 Vue.config.productionTip = false
